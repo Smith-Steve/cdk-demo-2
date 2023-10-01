025102676222cbe8cd039e4148a997d4115b05bb
smith-steve <Steve.A.Dore@outlook.com>
2023-09-30 21:23:48 -0700

    first commit

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -5,6 +5,7 @@ import { CdkDemoStack } from '../lib/cdk-demo-stack';
 
 const app = new cdk.App();
 new CdkDemoStack(app, 'CdkDemoStack', {
+  env: { account: '531698586584', region: 'us-east-1'}
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkDemoStack extends cdk.Stack {
@@ -7,6 +8,18 @@ export class CdkDemoStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    new ec2.Vpc(this, 'mainVPC', {
+      maxAzs: 2,
+      //this is where the subnect configurations are defined.
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public-subnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      ]
+    })
 
     // example resource
     // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
